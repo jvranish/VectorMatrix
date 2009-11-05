762437be8944be2a5772038139cfdce03f7efd6b
Job Vranish <jvranish@gmail.com>
2009-11-04 19:42:28 -0500

    started polishing

--- Data/Matrix.hs
+++ Data/Matrix.hs
@@ -6,19 +6,35 @@ module Data.Matrix where
 
 import Control.Applicative
 import Control.Monad
+import Data.Maybe
 import Data.Monoid
 import Data.Foldable
 import Data.Traversable
-import Data.List hiding (mapAccumL, sum, concat)
+import Data.List hiding (mapAccumL, sum, product, concat, concatMap, transpose)
+import qualified Data.List
 
 import Data.Lenses
-import Data.ZipList hiding (diagonal)
 import Data.Vector
 import Control.Monad.State hiding (sequence_, sequence)
 
-import Prelude hiding (sum, concat, sequence_, sequence)
+import Prelude hiding (sum, product, concat, concatMap, sequence_, sequence)
 
-data Matrix row col a = Matrix { unMatrix :: (row (col a))} deriving (Show, Eq, Ord)
+data Matrix row col a = Matrix (row (col a)) deriving (Show, Eq, Ord)
+
+unMatrix (Matrix m) = m
+
+--instance (Show (row (col a))) => Show (Matrix row col a) where
+--  show (Matrix m) = "Matrix (" ++ show m ++ ")"
+
+showMatrix (Matrix m) = concatMap showRow $ toList m
+  where
+    showRow row = "| " ++ concat ((intersperse " ") (fmap show (toList row))) ++ " |\n"
+--  | 1 2 |
+--  | 2 3 |
+
+
+--myShow (Matrix m) = concatMap showRow $ toList m
+
 
 inMatrix :: (t (f a) -> row (col b)) -> Matrix t f a -> Matrix row col b
 inMatrix f (Matrix a) = Matrix (f a)
@@ -76,6 +92,150 @@ instance (Fractional a, Applicative row, Applicative col, Eq (row (col a)), Show
   recip a = pure 1 / a
   fromRational = pure . fromRational
 
+gaussMatrix :: ( Traversable row, Applicative row,
+        Traversable col1, Applicative col1,
+        Traversable col2, Applicative col2,
+        Fractional a ) =>
+      Matrix row col1 a -> Matrix row col2 a -> GaussResult (Matrix row col1 a, Matrix row col2 a)
+gaussMatrix a b = do
+    --(primary, augmented) = gaussianElimination (toMatrixList a) (toMatrixList b)
+    --a' = fromJust $ snd $ castContainer' (head $ head primary) (concat primary)
+    -- b' = fromJust $ snd $ castContainer' (head $ head augmented) (concat augmented)
+    result <- gaussianElimination $ zipWith (++) (toMatrixList a) (toMatrixList b)
+    let big = concat $ Data.List.transpose $ reduceEchelon $ result
+    let (xs, a') = mapSnd fromJust $ castContainer' (head big) big
+    let b' = fromJust $ snd $ castContainer' (head big) xs
+    return (transpose a', transpose b')
+
+--castContainer'
+toMatrixList (Matrix a) = toList $ fmap toList a
+
+mapSnd f (a, b) = (a, f b)
+
+castContainer' :: (Foldable f, Traversable g, Applicative g) =>  a -> f a -> ([a], Maybe (g a))
+castContainer' a t = mapSnd sequenceA $ mapAccumL f (toList t) (pure a)
+  where
+    f [] _ = ([], Nothing)
+    f (x:xs) _ = (xs, Just x)
+
+det :: (Fractional a, Foldable f, Functor f) => Matrix f f a -> a
+det a = product $ zipWith id diagonalGets $ whatever $ gaussianElimination $ toMatrixList a
+
+inv :: ( Fractional a,
+         Traversable row, Monad row, Applicative row,
+         Traversable col1, Applicative col1) =>
+       Matrix row col1 a -> Maybe (Matrix row row a)
+inv a = toMaybe $ fmap snd $ gaussMatrix a identity
+
+toMaybe :: GaussResult a -> Maybe a
+toMaybe (Solved x) = Just x
+toMaybe (Underconstrained _) = Nothing
+
+-- The gaussianElimination was graciously provided by Keith Sheppard
+-- (with a few minor modifications...)
+data GaussResult a = Solved a
+                   | Underconstrained a
+             deriving (Show, Eq)
+
+instance Functor GaussResult where
+  fmap f (Solved x) = Solved (f x)
+  fmap f (Underconstrained x) = Underconstrained (f x)
+
+instance Monad GaussResult where
+  return a = Solved a
+  (Solved a) >>= f = f a
+  (Underconstrained a) >>= f = Underconstrained $ whatever $ f a
+
+instance Applicative GaussResult where
+  pure = return
+  (<*>) = ap
+
+whatever :: GaussResult a -> a
+whatever (Solved x) = x
+whatever (Underconstrained x) = x
+
+{-
+--withVector :: f a -> (Matrix f Vector1 -> Matrix f Vector1) -> f a
+withVector v f = case f $ Matrix $ Vector1 v of
+                   Matrix (Vector1 result) -> result
+
+
+rotMatrix2x2 angle = Matrix $ Vector2 (Vector2 c (-s)) (Vector2 s c)
+  where
+    s = sin angle
+    c = cos angle
+
+rotMatrix3x3 angle (Vector3 x y z)
+
+translateMatrix (Vector3 x y z)
+
+scaleMatrix v
+
+rotMatrix4x4 angle (Vector3 x y z)
+
+skewMatrix (Vector3 x y z) =
+-}
+--gaussianElimination :: (Fractional a) => [[a]] -> GaussResult [[a]]
+--gaussianElimination xss = case gaussianElimination' xss of
+--                              (True, result) -> Right result
+
+gaussianElimination :: (Fractional a) => [[a]] -> GaussResult [[a]]
+gaussianElimination [] = Underconstrained []
+gaussianElimination [[]] = Underconstrained [[]]
+gaussianElimination coefMat = do -- yay for monads!
+    -- work on zeroing out the leftmost coefficients in all equations
+    -- below the top (ie below topCoefs). The ensureNonZeroFirstCoef
+    -- function helps us deal with the special case where the first
+    -- coefficient is zero
+    topCoefs:otherCoefsMat <- ensureNonZeroFirstCoef coefMat
+    let zeroOutFactors = map (zeroOutFactor topCoefs) otherCoefsMat
+
+    let zeroOutCoefMatAddends = zipWith (map . (*)) zeroOutFactors (repeat topCoefs)
+    let zeroedOutCoefMat = zipWith (zipWith (+)) otherCoefsMat zeroOutCoefMatAddends
+
+    -- now that the leftmost coefs are zero we need to recurse into the
+    -- lower right corner
+    let lowerRightMat = map tail zeroedOutCoefMat
+    newLowerRightMat <- gaussianElimination lowerRightMat
+
+    -- OK now we just have to zip it up like we need
+    let newLowerMat = zipWith (:) (map head zeroedOutCoefMat) newLowerRightMat
+    return $ topCoefs:newLowerMat
+
+-- this function is just to make sure that the left-most coefficient is non-zero. If the 1st leftmost coefficient is zero in the 1st row then that row is swapped with the 1st row that has a non-zero starting coefficient
+
+ensureNonZeroFirstCoef coefMat =
+  let firstNonZeroIndex = findIndex ((/= 0) . head) coefMat
+  in case firstNonZeroIndex of
+      Just 0  -> Solved $ coefMat
+      Just i  -> Solved $ zeroSwap i coefMat
+      Nothing -> Underconstrained coefMat -- error "Failed to find non-zero coefficient!"
+  where zeroSwap i xs =
+          let (start, finish) = splitAt i xs
+          in head finish : start ++ tail finish
+
+--the zeroOutFactor determines the factor that should be used to zero out the left-most coefficient in otherCoefs
+
+zeroOutFactor :: (Fractional a) => [a] -> [a] -> a
+zeroOutFactor topCoefs otherCoefs =
+  let topLeftCoef = head topCoefs
+      otherLeftCoef = negate $ head otherCoefs
+  in if topLeftCoef /= 0 then otherLeftCoef / topLeftCoef else otherLeftCoef
+
+
+getUppers xs = zipWith fmap diagonalGets (tail $ inits xs)
+diagonalGets = iterate (. tail) head
+reduceEchelon = reduceRows diagonalGets []
+
+reduceRows _ previousRows []  = previousRows
+reduceRows (f:fs) previousRows (row:rows) = reduceRows fs (reducedRows ++ [scaledRow]) rows
+  where
+    -- scale our row so diagonal is 1
+    scaledRow = fmap (/ (f row)) row
+    -- zero out the rest of that column
+    reducedRows = fmap (reduce scaledRow) previousRows
+    reduce sRow pRow = zipWith (-) pRow (fmap (* (f pRow)) sRow)
+
 
 a = Matrix (Vector3 (Vector3 0 1 3)
                     (Vector3 4 5 6)
@@ -86,6 +246,14 @@ g 9 = Matrix (Vector3 (Vector3 0 0 0)
 g x = Matrix (Vector3 (Vector3 0 0 0)
                       (Vector3 0 0 0)
                       (Vector3 0 0 0) )
+
+b = Matrix (Vector2 (Vector3 0 1 3)
+                    (Vector3 4 5 6) )
+h 6 = Matrix (Vector2 (Vector3 0 0 0)
+                      (Vector3 0 0 1) )
+h x = Matrix (Vector2 (Vector3 x x x)
+                      (Vector3 x x x) )
+
 --  xs >>=
 --m (m a) f
 --pure f
@@ -139,7 +307,7 @@ Vector ()
 {-
 
 
-OMG a very much work in progress 
+OMG a very much work in progress
 
 
 
@@ -340,6 +508,16 @@ castContainer t = sequenceA $ snd $ mapAccumL f (toList t) (pure undefined)
     f [] _ = ([], Nothing)
     f (x:xs) _ = (xs, Just x)
 
+
+
+{-
+castContainer' :: (Foldable f, Applicative g, Traversable g) => f a -> (Maybe (g a), [a])
+castContainer' t = (sequenceA $ snd $ results, fst results)
+  where
+    results = mapAccumL f (toList t) (pure undefined)
+    f [] _ = ([], Nothing)
+    f (x:xs) _ = (xs, Just x)
+-}
 {-
 
 matrix
